754a3a7aee4176d55d501a32874b5bafac6895ca
stancecoke <hochsitzcola@web.de>
2021-06-08 11:04:59 +0200

    switched to vertical orientation

--- TFT Test/Core/Src/main.c
+++ TFT Test/Core/Src/main.c
@@ -30,7 +30,7 @@
 #include "ILI9341_STM32_Driver.h"
 #include "ILI9341_GFX.h"
 #include <stdio.h>
-#include "snow_tiger.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -58,6 +58,7 @@ uint16_t i;
 uint16_t percent = 0;
 uint16_t tempPercent;
 uint16_t tempClock;
+uint16_t batsympos[2]= {190,5};
 char trans_str[64] = {0,};
 char Temp_Buffer_text[40];
 /* USER CODE END PV */
@@ -110,7 +111,7 @@ int main(void)
   /* USER CODE BEGIN 2 */
   ILI9341_Init();//initial driver setup to drive ili9341
   ILI9341_Fill_Screen(BLACK);
-  ILI9341_Set_Rotation(SCREEN_HORIZONTAL_1);
+  ILI9341_Set_Rotation(SCREEN_VERTICAL_1);
   ILI9341_DrawText("EBiCS Display", FONT3, 10, 25, WHITE, BLACK); //DrawText uses Fonts from fonts.h / fonts.c
   ILI9341_DrawBigNumber("456", BIGFONT,10, 50, WHITE, BLACK);
   //ILI9341_DrawText("123", BIGFONT, 10, 40, WHITE, BLACK);
@@ -207,21 +208,21 @@ void checkBattery()
 	          HAL_ADC_PollForConversion(&hadc1, 100); // ожидаем окончани�? преобразовани�?
 	          adc = HAL_ADC_GetValue(&hadc1); // читаем полученное значение в переменную adc
 	          //HAL_ADC_Stop(&hadc1); // о�?танавливаем �?ЦП
-	          j = map(adc, 0, 4096, 256, 290);
+	          j = map(adc, 0, 4096, batsympos[0]+4, batsympos[0]+38); //(adc, 0, 4096, 256, 290);
 	          percent = map(adc, 0, 4096, 0, 100);
 	         if(j<=273 && j>=260){i=ORANGE;};
 	         if(j>273){i= GREEN;}
 	         if(j<260){i= RED;}
 	snprintf(trans_str, 63, "%d\n", percent);
-	ILI9341_Draw_Hollow_Rectangle_Coord(252, 4, 291, 18, WHITE);
-			ILI9341_Draw_Hollow_Rectangle_Coord(253, 5, 290, 17, WHITE);
-			ILI9341_Draw_Filled_Rectangle_Coord(292, 8, 295, 15, WHITE);
+	ILI9341_Draw_Hollow_Rectangle_Coord(batsympos[0], batsympos[1], batsympos[0]+39, batsympos[1]+14, WHITE);  //(252, 4, 291, 18, WHITE);
+			ILI9341_Draw_Hollow_Rectangle_Coord(batsympos[0]+1, batsympos[1]+1, batsympos[0]+38, batsympos[1]+13, WHITE); //(253, 5, 290, 17, WHITE);
+			ILI9341_Draw_Filled_Rectangle_Coord(batsympos[0]+40, batsympos[1]+4, batsympos[0]+43, batsympos[1]+11, WHITE); //(292, 8, 295, 15, WHITE);
 	if(tempPercent!=percent){
-		ILI9341_Draw_Filled_Rectangle_Coord(254, 6, 290, 17, i);
-		ILI9341_Draw_Filled_Rectangle_Coord(j, 17, 290, 6, BLACK);
+		ILI9341_Draw_Filled_Rectangle_Coord(batsympos[0]+2, batsympos[1]+2, batsympos[0]+38, batsympos[1]+13, i);//(254, 6, 290, 17, i);
+		ILI9341_Draw_Filled_Rectangle_Coord(j, batsympos[1]+13, batsympos[0]+38, batsympos[1]+2, BLACK);//(j, 17, 290, 6, BLACK);
 		tempPercent=percent;
-		ILI9341_DrawText(trans_str, FONT3, 216, 5, WHITE, BLACK);
-		ILI9341_DrawText("%", FONT3, 237, 5, WHITE, BLACK);
+		ILI9341_DrawText(trans_str, FONT3, batsympos[0]-35, batsympos[1], WHITE, BLACK);
+		ILI9341_DrawText("%", FONT3, batsympos[0]-20,  batsympos[1], WHITE, BLACK);
 	}
 }
 void clock()
